8bb5a61b6fe8a660fa940ade839e56e52ad42a63
ming <xiaoming19210@163.com>
2015-03-12 11:45:31 +0800

    更改下测试

--- soap-client/src/test/java/com/ming/soap/wsdl/support/SoapClientTest.java
+++ soap-client/src/test/java/com/ming/soap/wsdl/support/SoapClientTest.java
@@ -1,8 +1,9 @@
 package com.ming.soap.wsdl.support;
 
 import org.junit.Test;
-
-
+import static org.junit.Assert.*;
+import com.ming.soap.SoapClient;
+import com.ming.soap.SoapClientException;
 import com.ming.soap.pojo.Element;
 import com.ming.soap.pojo.Message;
 import com.ming.soap.pojo.RetInfo;
@@ -10,34 +11,28 @@ import com.ming.soap.pojo.WsdlEntity;
 import com.ming.soap.pojo.WsdlEntitys;
 import com.ming.soap.xml.parser.WsdlEntityParser;
 
-/**
- * SoapClient 的例子，两种不同的调用方式
- * 1. 依赖wsdlEntity.xml 获取WsdlEntity实例的调用
- * 2. 不依赖配置获得WsdlEntity实例的调用
- * */
 public class SoapClientTest {
 	private  SoapClient client = SoapClient.newInstance();
 	private WsdlEntityParser parser = WsdlEntityParser.newInstance();
-	/**
-	 * 依赖wsdlEntity.xml 获取WsdlEntity实例的调用
-	 * 可以再xml中灵活的配置 soap-client请求的各种参数
-	 * */
+	
 	@Test
 	public void test01 () throws SoapClientException {
 		WsdlEntity  wsdl = parser.getWsdlEntity("GetInfoServiceImplService");
-		Element[] argument = {new Element("retInfo", new RetInfo("name", 1), RetInfo.class)};
+		RetInfo ri = new RetInfo("name", 1);
+		Element[] argument = {new Element("retInfo", ri, RetInfo.class)};
 		RetInfo retInfo = (RetInfo)client.invoke(wsdl, new Message("result", "http://inf.service.app.demo.sysware.com/", "getRetInfoObj", "nn", argument), RetInfo.class);
-		System.out.println(retInfo.getAge() + retInfo.getName());
+		assertEquals(retInfo.getAge(), ri.getAge());
+		assertEquals(retInfo.getName(), ri.getName());
 	}
-	/**
-	 * 不依赖配置获得WsdlEntity实例的调用
-	 * */
+	
 	@Test
 	public void test02 () throws SoapClientException {
-		WsdlEntity  wsdl = WsdlEntitys.newInstance().createWsdlEntity("http://localhost:8080/soap-server/ws/getInfoService?wsdl",
+		WsdlEntity  wsdl = WsdlEntitys.newInstance().createWsdlEntity("http://localhost:8989/soap-server/ws/getInfoService?wsdl",
 				"http://impl.service.app.demo.sysware.com/", "GetInfoServiceImplService", "GetInfoServiceImplPort");
-		Element[] argument = {new Element("retInfo", new RetInfo("name", 1), RetInfo.class)};
+		RetInfo ri = new RetInfo("name", 1);
+		Element[] argument = {new Element("retInfo", ri, RetInfo.class)};
 		RetInfo retInfo = (RetInfo)client.invoke(wsdl, new Message("result", "http://inf.service.app.demo.sysware.com/", "getRetInfoObj", "nn", argument), RetInfo.class);
-		System.out.println(retInfo.getAge() + retInfo.getName());
+		assertEquals(retInfo.getAge(), ri.getAge());
+		assertEquals(retInfo.getName(), ri.getName());
 	}
 }
